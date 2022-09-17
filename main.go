887f7f5ceeb5cc46a975c601798c702b7a389606
Roch D'Amour <roch.damour@gmail.com>
2022-09-16 21:02:47 -0400

    Filter with a typo

--- main.go
+++ main.go
@@ -2,10 +2,25 @@ package main
 
 import (
 	"fmt"
+	"strings"
+
 	"github.com/notarock/technews-bot/pkg/sources/hackernews"
+	"github.com/peterhellberg/hn"
 )
 
 func main() {
 	i := hackernews.FetchLatest(10)
-	fmt.Println(i)
+	keywords := []string{"game"}
+	for _, item := range i {
+		filder(item, keywords)
+	}
+}
+
+func filder(item *hn.Item, keywords []string) {
+	for _, keyword := range keywords {
+		if strings.Contains(strings.ToLower(item.Title), keyword) {
+			fmt.Println(item.Title)
+		}
+	}
+
 }
