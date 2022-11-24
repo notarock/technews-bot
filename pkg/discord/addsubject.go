fce7c1f2ec22ff06a43d0a45feedae490f952b85
ThomasCardin <tomcardin@outlook.com>
2022-11-24 17:22:54 -0500

    Add technews subject to a bound channel

---
+++ pkg/discord/addsubject.go
@@ -0,0 +1,37 @@
+package discord
+
+import (
+	"github.com/bwmarrin/discordgo"
+	"github.com/notarock/technews-bot/pkg/database"
+	"strings"
+)
+
+func addSubjectToChannel(s *discordgo.Session, m *discordgo.MessageCreate) discordgo.MessageEmbed {
+	guild, err := s.Guild(m.GuildID)
+	if err != nil {
+		return ErrorEmbed
+	}
+
+	guilds, err := database.GetAllGuilds()
+	if err != nil {
+		return ErrorEmbed
+	}
+
+	for _, g := range guilds {
+		if g.GuildID == guild.ID && g.Settings.ChannelID == m.ChannelID {
+			contentWithoutCmd := strings.ReplaceAll(m.Content, "!technews addsubject ", "")
+			_, err := database.AddSubjectToGuild(g, contentWithoutCmd)
+			if err != nil {
+				return ErrorEmbed
+			}
+
+			return discordgo.MessageEmbed{
+				Title: "Subject added to channel successfully!",
+			}
+		}
+	}
+
+	return discordgo.MessageEmbed{
+		Title: "Cannot bind the subject.",
+	}
+}
