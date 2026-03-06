dcecab39396db5abcf886c77f79fd80ed6ba0a18
Roch D'Amour <roch.damour@gmail.com>
2026-03-05 22:14:46 -0500

    Serving x guilds

--- pkg/database/client.go
+++ pkg/database/client.go
@@ -4,6 +4,8 @@ import (
 	"context"
 	"time"
 
+	"github.com/sirupsen/logrus"
+	"go.mongodb.org/mongo-driver/bson"
 	"go.mongodb.org/mongo-driver/mongo"
 	"go.mongodb.org/mongo-driver/mongo/options"
 )
@@ -51,6 +53,11 @@ func Healthcheck() error {
 	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
 	defer cancel()
 
-	_, err := collections.Guild.CountDocuments(ctx, nil)
-	return err
+	count, err := collections.Guild.CountDocuments(ctx, bson.M{})
+	if err != nil {
+		return err
+	}
+
+	logrus.Infof("Serving %d guilds", count)
+	return nil
 }
